8e2e0205d5c04e02ef8f7a91edc636ec5fdfed64
Tay Kai Yang <taykaiyang556@gmail.com>
2021-01-09 03:51:34 +0800

    added google apis

---
+++ lib/secrets.dart
@@ -0,0 +1,13 @@
+class Secret {
+  static const ANDROID_CLIENT_ID = "313558371511-a5436ts63nsug5g81ml357r63083u97b.apps.googleusercontent.com";
+
+  static String getId() => Secret.ANDROID_CLIENT_ID;
+}
+
+const API_KEY = 'AIzaSyD3RhzJymL9nM-V1hTr5kqHXpnoVrM7jc4';
+const BASE_URL = 'https://texttospeech.googleapis.com/v1/';
+const VOICE_NAME = 'en-US-Wavenet-D';
+const AUDIO_ENCODING = 'MP3';
+const LANGUAGE_CODE = 'en-us';
+//const SSML_GENDER = 'MALE';
+const END_POINT = 'text:synthesize';
